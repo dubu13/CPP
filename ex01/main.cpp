091ce15051caaf5ab83583aa1ad7b61e57e8f715
Dilara Mutlu <dilara.hasan1301@gmail.com>
2024-07-29 01:00:13 +0200

    main loop

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 class PhoneBook
 {
 	public:
@@ -18,4 +19,23 @@ class PhoneBook
 class Contsct
 {
 	public:
+}
+
+int main()
+{
+	std::string command;
+
+	std::cout << "Welcome to the phonebook!" << std::endl;
+	std::cout << "You can use the following commands: ADD, SEARSCH, EXIT." << std::endl;
+	while (1)
+	{
+		std::cout << "Please enter a command: ";
+		std::getline(std::cin, command);
+		if (command == "ADD")
+		else if (command == "SEARCH")
+		else if (command == "EXIT")
+			exit(0);
+		else
+			std::cout << "Invalid command, use ADD, SEARCH or EXIT." << std::endl;
+	}
 }
