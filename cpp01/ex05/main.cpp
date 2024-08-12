9f35fbbbf4afc0b2350faf801fbf34e3c28bceb2
dubu13 <dilara.hasan1301@gmail.com>
2024-08-12 15:49:55 +0200

    done w ex05

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhasan <dhasan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 14:41:28 by dhasan            #+#    #+#             */
+/*   Updated: 2024/08/12 15:45:24 by dhasan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	harl.complain("TEST");
+	harl.complain("");
+
+	return 0;
+}
