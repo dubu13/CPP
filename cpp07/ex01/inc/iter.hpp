0ba9468f1e249416286ddac36b75021605e899d4
dubu13 <dilara.hasan1301@gmail.com>
2024-12-10 20:07:21 +0100

    testing template funcs with class type

--- cpp07/ex01/inc/iter.hpp
+++ cpp07/ex01/inc/iter.hpp
@@ -2,7 +2,6 @@
 #include <iostream>
 
 template <typename T>
-
 void iter(T *array, size_t len, void (*func)(T &)) {
 
     for (size_t i = 0; i < len; i++)
--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -1,5 +1,17 @@
 #include "iter.hpp"
 
+class Awesome{
+    public:
+        Awesome( void ) : _n( 42 ) { return; }
+        int get( void ) const { return this->_n; }
+    private:
+        int _n;
+};
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
 int main(){
 
     int arr1[] = {1, 2, 3, 4, 5};
@@ -7,6 +19,12 @@ int main(){
 
     std::string arr2[] = {"Hello", "World", "!"};
     iter(arr2, 3, print);
+
+    int tab[] = { 0, 1, 2, 3, 4 };
+    Awesome tab2[5];
+
+    iter( tab, 5, print );
+    iter( tab2, 5, print );
 
     return 0;
 }
