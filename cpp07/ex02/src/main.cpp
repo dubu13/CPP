b844b59df592f7bdbc7bcc0642aa9b1b1f217039
dubu13 <dilara.hasan1301@gmail.com>
2024-12-10 19:55:12 +0100

    extra main func for testing

--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -17,6 +17,8 @@ int main(int, char**)
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
+        // std::cout << "tmp: " << tmp[0] << std::endl;
+        // std::cout << "test: " << test[0] << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -51,3 +53,72 @@ int main(int, char**)
     delete [] mirror;//
     return 0;
 }
+
+// class Test{
+//     public:
+//         Test(): _n(0){}
+//         ~Test() = default;
+//         int getN() const { return _n; }
+//         void setN(int n) { _n = n; }
+//     private:
+//         int _n;
+// };
+
+// int main(){
+
+//     std::cout << "---empty array---" << std::endl;
+//     try{
+//         Array<int> empty;
+//         std::cout << "size: " << empty.size() << ", member: " << empty[0] << std::endl;
+//     }
+//     catch(const std::exception& e){
+//         std::cerr << e.what() << '\n';
+//     };
+//     std::cout << "---array---" << std::endl;
+//     try{
+//         Array<int> arr(3);
+//         std::cout << "size: " << arr.size() << std::endl;
+//         for (unsigned int i = 0; i < arr.size(); i++){
+//             arr[i] = i;
+//             std::cout << "arr[" << i << "]: " << arr[i] << std::endl;
+//         }
+//     }
+//     catch(const std::exception& e){
+//         std::cerr << e.what() << '\n';
+//     }
+//     std::cout << "---const array---" << std::endl;
+//     try{
+//         const Array<double> arr(3);
+//         std::cout << "size: " << arr.size() << std::endl;
+//         for (unsigned int i = 0; i < arr.size(); i++)
+//             std::cout << "arr[" << i << "]: " << arr[i] << std::endl;
+//     }
+//     catch(const std::exception& e){
+//         std::cerr << e.what() << '\n';
+//     }
+//     std::cout << "---copy array---" << std::endl;
+//     try{
+//         Array<char> arr(3);
+//         for(unsigned int i = 0; i < arr.size(); i++){
+//             arr[i] = 'a' + i;
+//             std::cout << "arr[" << i << "]: " << arr[i] << std::endl;
+//         }
+//         Array<char> arr2(arr);
+//         for(unsigned int i = 0; i < arr2.size(); i++)
+//             std::cout << "arr2[" << i << "]: " << arr2[i] << std::endl;
+//     }
+//     catch(const std::exception& e){
+//         std::cerr << e.what() << '\n';
+//     }
+//     std::cout << "---class array---" << std::endl;
+//     try{
+//         Array<Test> arr(3);
+//         for(unsigned int i = 0; i < arr.size(); i++){
+//             arr[i].setN(i);
+//             std::cout << "arr[" <<  i << "]: " << arr[i].getN() << std::endl;
+//         }
+//     }
+//     catch(const std::exception& e){
+//         std::cerr << e.what() << '\n';
+//     }
+// }
