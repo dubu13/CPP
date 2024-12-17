ca0e745bab9c4b9f0a2de92378ee25400e5dda1c
dubu13 <dilara.hasan1301@gmail.com>
2024-12-18 00:07:13 +0100

    adding iterator support to mutantstack class

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -6,6 +6,20 @@
 template <typename T>
 class MutantStack : public std::stack<T>{
     public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+        iterator begin(){return this->c.begin();};
+        iterator end(){return this->c.end();};
+        reverse_iterator rbegin(){return this->c.rbegin();};
+        reverse_iterator rend(){return this->c.rend();};
+        const_iterator cbegin(){return this->c.cbegin();};
+        const_iterator cend(){return this->c.cend();};
+        const_reverse_iterator crbegin(){return this->c.crbegin();};
+        const_reverse_iterator crend(){return this->c.crend();};
+    
         MutantStack(){};
         MutantStack(const MutantStack &other) : std::stack<T>(other){};
         MutantStack &operator=(const MutantStack &other){
