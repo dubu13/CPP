94990905d8c56f9f1570a5a6227e87a5d35a619a
dubu13 <dilara.hasan1301@gmail.com>
2024-12-18 13:00:39 +0100

    more test

--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,5 +1,8 @@
 #include "MutantStack.hpp"
+#include <list>
+
 void subject_test()
+{
     std::cout << "---Subject test---" << std::endl;
     MutantStack<int> mstack;
 
@@ -26,4 +29,56 @@ void subject_test()
         ++it;
     }
     std::stack<int> s(mstack);
+}
+
+void compare_test()
+{
+    std::cout << "---Compare test w list---" << std::endl;
+    std::list<int> lst;
+
+    lst.push_back(5);
+    lst.push_back(17);
+    std::cout << lst.back() << std::endl;
+
+    lst.pop_back(); 
+    std::cout << lst.size() << std::endl;
+
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+
+    std::list<int>::iterator it = lst.begin();
+    std::list<int>::iterator ite = lst.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::list<int> s(lst);
+}
+
+void forward_reverse_test()
+{
+    MutantStack<char> mstack;
+    mstack.push('a');
+    mstack.push('b');
+    mstack.push('c');
+
+    std::cout << "---forward iterator---" << std::endl;
+    for (MutantStack<char>::iterator it = mstack.begin(); it != mstack.end(); it++)
+        std::cout << *it << std::endl;
+    std::cout << "---reverse iterator---" << std::endl;
+    for (MutantStack<char>::reverse_iterator it = mstack.rbegin(); it != mstack.rend(); it++)
+        std::cout << *it << std::endl;
+}
+
+int main()
+{
+    subject_test();
+    compare_test();
+    forward_reverse_test();
+    return 0;
 }
